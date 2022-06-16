b44a34a8505d53847b6201278a710235eabe7869
Jayeskumar <52567903+Jayeskumar@users.noreply.github.com>
2022-06-16 16:15:00 +0000

    Pending changes exported from your codespace

--- Terv.java
+++ Terv.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Terv {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a=sc.nextInt();
+        int a=9;
         System.out.println(Math.sqrt(a));
     }
 }
